83646e386e11f70957c68b1eee47157d65a745d0
Lusif <Lusif.yt@gmail.com>
2023-04-13 21:23:48 -0400

    Added 1st Script

---
+++ SaccFlightAddons/RepairToolTrigger/RepairToolTrigger.cs
@@ -0,0 +1,36 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+
+namespace SaccFlightAndVehicles
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class RepairToolTrigger : UdonSharpBehaviour
+    {
+        [Tooltip("Object to send event to")]
+        public SaccGroundVehicle VehicleCore;
+        [Tooltip("Object to send wakeup to")]
+        public SGV_EffectsController VehicleEffects;
+        [Tooltip("Name of event sent by this trigger")]
+        public string EventName = "ReSupply";
+
+        public void OnParticleCollision()
+        {
+            Networking.SetOwner(Networking.LocalPlayer, this.gameObject);
+            SendRepair();
+        }
+
+        public void SendRepair()
+        {
+            VehicleCore.SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, EventName);
+            VehicleEffects.SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "WakeUp");
+        }
+
+
+    }
+}
+
